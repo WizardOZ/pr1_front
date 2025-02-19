a0e3d7253a5621912d7f9c66a9c583b08b857d15
WizardOz <jbg_jeboga@hotmail.com>
2025-02-19 09:02:54 +0100

    V6_Recheck_css

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -19,11 +19,11 @@ export default function Counter(props: CounterProps) {
           <h3>Máster en Ciencia de Datos y Machine Learning</h3>
           <h4>Universidad de Stanford | 2016 - 2018</h4>
           <p>Especialización en modelos predictivos, aprendizaje profundo y procesamiento de grandes volúmenes de datos. Proyecto final enfocado en la detección de fraudes financieros mediante redes neuronales.</p>
-          
+          <br></br>
           <h3>Grado en Matemáticas y Estadística</h3>
           <h4>Universidad de Barcelona | 2008 - 2012</h4>
           <p>Formación en estadística avanzada, optimización y modelado matemático aplicado a la ciencia de datos.</p>
-          
+          <br></br>
           <h3>Certificación en Big Data y Computación en la Nube</h3>
           <h4>Google Cloud & Coursera | 2019</h4>
           <p>Desarrollo y despliegue de modelos escalables en entornos cloud con TensorFlow y Google BigQuery.</p>
