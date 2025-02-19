4e9eb2ac72f01ad4176788384d88def19eaf5a54
WizardOz <jbg_jeboga@hotmail.com>
2025-02-19 07:32:42 +0100

    V1

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -7,9 +7,9 @@ interface CounterProps {
 
 export default function Counter(props: CounterProps) {
   return (
-    <div class="flex flex-col gap-8 py-6">
+    <div>
       <Button onClick={() => props.count.value = !props.count.value}>mostrar mas</Button>
-      {props.count.value &&  <p class="text-3xl tabular-nums ">Máster en Ciencia de Datos y Machine Learning <br></br> "Universidad de Stanford | 2016 - 2018<br></br>Especialización en modelos predictivos, aprendizaje profundo y procesamiento de grandes volúmenes de datos. Proyecto final enfocado en la detección de fraudes financieros mediante redes neuronales.<br></br><br></br>Grado en Matemáticas y Estadística<br></br>Universidad de Barcelona | 2008 - 2012<br></br>Formación en estadística avanzada, optimización y modelado matemático aplicado a la ciencia de datos.<br></br><br></br>Certificación en Big Data y Computación en la Nube<br></br>Google Cloud & Coursera | 2019<br></br>Desarrollo y despliegue de modelos escalables en entornos cloud con TensorFlow y Google BigQuery."</p>
+      {props.count.value &&  (<div class = "counter"><p><h3>Máster en Ciencia de Datos y Machine Learning</h3> <h4>"Universidad de Stanford | 2016 - 2018</h4>Especialización en modelos predictivos, aprendizaje profundo y procesamiento de grandes volúmenes de datos. Proyecto final enfocado en la detección de fraudes financieros mediante redes neuronales.<br></br><br></br><h3>Grado en Matemáticas y Estadística</h3><h4>Universidad de Barcelona | 2008 - 2012</h4>Formación en estadística avanzada, optimización y modelado matemático aplicado a la ciencia de datos.<br></br><br></br><h3>Certificación en Big Data y Computación en la Nube</h3><h4>Google Cloud & Coursera | 2019</h4>Desarrollo y despliegue de modelos escalables en entornos cloud con TensorFlow y Google BigQuery."</p></div>)
     }
     </div>
   );
